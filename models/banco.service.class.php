6752dd9f3966881ef8a119fec27d39dc66fc1d36
Davi Figueiredo <127113964+DaviFigs@users.noreply.github.com>
2026-05-08 15:57:15 -0400

    Update banco.service.class.php

--- models/banco.service.class.php
+++ models/banco.service.class.php
@@ -1,24 +1,32 @@
 <?php
 require_once __DIR__ . '/../phpConfig.php';
 
-class BANCO{
+class BANCO {
 
-    static function conectar(){
-        try{
-            $con = new PDO(
-                'pgsql:dbname=' . $_ENV['DB_NAME'] . ';host=' . $_ENV['DB_HOST'],
-                $_ENV['DB_USER'],
-                $_ENV['DB_PASS']
-            );
+    static function conectar() {
+        try {
 
-            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $url = $_ENV['DATABASE_URL'];
+
+            // quebra a URL do Supabase
+            $db = parse_url($url);
+
+            $host = $db['host'];
+            $user = $db['user'];
+            $pass = $db['pass'];
+            $dbname = ltrim($db['path'], '/');
+
+            $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;sslmode=require";
+
+            $con = new PDO($dsn, $user, $pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
 
             return $con;
-        }
-        catch(Exception $e){
+
+        } catch (Exception $e) {
             echo "Erro Conexão: " . $e->getMessage();
             return null;
         }
     }
-
 }
